968ef0706d626023e9d1a4db36ec0de93847ed09
Gustavo Kimura <gustavokimuraf@gmail.com>
2023-02-21 16:02:23 -0300

    refactor: removes Velocity trait

--- public/js/entities.js
+++ public/js/entities.js
@@ -1,5 +1,4 @@
 import Entity from './Entity.js';
-import Velocity from './traits/Velocity.js';
 import Jump from './traits/Jump.js';
 import Go from './traits/Go.js';
 import { loadMarioSprite } from './sprites.js';
@@ -13,7 +12,6 @@ export async function createMario() {
 
   mario.addTrait(new Go());
   mario.addTrait(new Jump());
-  // mario.addTrait(new Velocity());
 
   mario.draw = function drawMario(context) {
     sprite.draw('idle', context, 0, 0);
