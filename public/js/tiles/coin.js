52aac5d9226e2c8fbc21f81db194afadc0e1168e
Gustavo Kimura <gustavokimuraf@gmail.com>
2023-03-10 12:14:07 -0300

    feat: added extra lives counter when coin counter reaches 100

--- public/js/tiles/coin.js
+++ public/js/tiles/coin.js
@@ -1,7 +1,7 @@
-import { Sides } from '../Entity.js';
-
 function handle({ entity, match, resolver }) {
   if (entity.player) {
+    entity.player.addCoins(1);
+
     const grid = resolver.matrix;
 
     grid.delete(match.indexX, match.indexY);
--- public/js/traits/Player.js
+++ public/js/traits/Player.js
@@ -1,6 +1,8 @@
 import { Trait } from '../Entity.js';
 import Stomper from '../traits/Stomper.js';
 
+const COIN_LIFE_THRESHOLD = 100;
+
 export default class Player extends Trait {
   constructor() {
     super('player');
@@ -13,5 +15,21 @@ export default class Player extends Trait {
     this.listen(Stomper.EVENT_STOMP, () => {
       this.score += 100;
     });
+  }
+
+  addCoins(count) {
+    this.coins += count;
+
+    if (this.coins >= COIN_LIFE_THRESHOLD) {
+      const lifeCount = Math.floor(this.coins / COIN_LIFE_THRESHOLD);
+
+      this.addLives(lifeCount);
+
+      this.coins = this.coins % COIN_LIFE_THRESHOLD;
+    }
+  }
+
+  addLives(count) {
+    this.lives += count;
   }
 }
