613a3a1e520a5255601c62e281d0630f6ae25c6c
Gustavo Kimura <gustavokimuraf@gmail.com>
2023-03-24 13:14:00 -0300

    fix: game only run when click on black screen happens

--- public/js/Timer.js
+++ public/js/Timer.js
@@ -1,19 +1,21 @@
 export default class Timer {
   constructor(deltaTime = 1 / 60) {
     let accumulatedTime = 0;
-    let lastTime = 0;
+    let lastTime = null;
 
     this.updateProxy = (time) => {
-      accumulatedTime += (time - lastTime) / 1000;
+      if (lastTime) {
+        accumulatedTime += (time - lastTime) / 1000;
 
-      if (accumulatedTime > 1) {
-        accumulatedTime = 1;
-      }
+        if (accumulatedTime > 1) {
+          accumulatedTime = 1;
+        }
 
-      while (accumulatedTime > deltaTime) {
-        this.update(deltaTime);
+        while (accumulatedTime > deltaTime) {
+          this.update(deltaTime);
 
-        accumulatedTime -= deltaTime;
+          accumulatedTime -= deltaTime;
+        }
       }
 
       lastTime = time;
